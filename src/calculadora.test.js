be72f3902e9d9b8d511eaf9b6b7fb75b6d41430c
p48102611 <p481026110@gmail.com>
2021-10-01 17:29:09 -0400

    Funcion suma devuelve numero

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,29 +1,33 @@
-describe("Totalizador ", () => {
-  it("deberia calcular para un cantidad", () => {
-    expect(calcularTotal(2, 1, "")).toEqual(2);
+describe("Calculadora ", () => {
+  it("deberia devolver el numero ingresado", () => {
+    expect(sumar(3)).toEqual(3);
   });
-  it("deberia calcular para un cantidad", () => {
-    expect(calcularTotal(2, 2, "")).toEqual(4);
-  });
-  it("deberia calcular para un estado por defecto", () => {
-    expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
-  });
-  it("deberia calcular para un estado por defecto", () => {
-    expect(calcularTotal(3, 2, "CA")).toEqual(6.495);
-  });
-  it("deberia calcular para un estado por defecto", () => {
-    expect(calcularTotal(3, 2, "UT")).toEqual(6.399);
-  });
+  // it("deberia calcular para un cantidad", () => {
+  //   expect(calcularTotal(2, 2, "")).toEqual(4);
+  // });
+  // it("deberia calcular para un estado por defecto", () => {
+  //   expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
+  // });
+  // it("deberia calcular para un estado por defecto", () => {
+  //   expect(calcularTotal(3, 2, "CA")).toEqual(6.495);
+  // });
+  // it("deberia calcular para un estado por defecto", () => {
+  //   expect(calcularTotal(3, 2, "UT")).toEqual(6.399);
+  // });
 });
 
-function impuestoEstado(estado) {
-  let impuestos = { CA: 0.0825, UT: 0.0665, "": 0 };
-  return impuestos[estado];
+function sumar(cadena){
+  return cadena;
 }
 
-function calcularTotal(cantidad, precio, estado) {
-  let subTotal = cantidad * precio;
-  let impuesto = impuestoEstado(estado);
-  subTotal = subTotal + subTotal * impuesto;
-  return subTotal;
-}
+// function impuestoEstado(estado) {
+//   let impuestos = { CA: 0.0825, UT: 0.0665, "": 0 };
+//   return impuestos[estado];
+// }
+
+// function calcularTotal(cantidad, precio, estado) {
+//   let subTotal = cantidad * precio;
+//   let impuesto = impuestoEstado(estado);
+//   subTotal = subTotal + subTotal * impuesto;
+//   return subTotal;
+// }
