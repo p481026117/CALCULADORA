0c0f0fc7b2d450042c11fbf4cebc4458a0bad05a
p48102611 <p481026110@gmail.com>
2021-10-02 01:29:01 -0400

    deberia devolver la suma de la [***][%]\n1***2000%1001,7000-9  ====>  10

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,39 +1,27 @@
 const reducer = (accumulator, curr) => accumulator + curr;
 
-describe("Calculadora ", () => {
+describe("Calculadora de Cadenas ", () => {
   it("deberia devolver el numero ingresado 3  ====>  3", () => {
-    expect(Operacion("3")).toEqual(3);
+    expect(CalcularCadena("3")).toEqual(3);
   });
   it("deberia devolver la suma de la 1,2  ====>  3", () => {
-    expect(Operacion("1,2")).toEqual(3);
+    expect(CalcularCadena("1,2")).toEqual(3);
   });
   it("deberia devolver la suma de la 1,2-3  ====>  6", () => {
-    expect(Operacion("1,2-3")).toEqual(6);
+    expect(CalcularCadena("1,2-3")).toEqual(6);
   });
-  // it("probando funcion delimitadores", () => {
-  //   expect(Resultado("", "[***,hh,y]")).toEqual(["***", "hh", "y"]);
-  // });
   it("probando funcion Resultado deberia devolver una cadena separada por comas", () => {
-    expect(Resultado("3***2hh1y1","[***][hh][y]")).toEqual("3,2,1,1");
+    expect(CadenaConDelimitadores("3***2hh1y1","[***][hh][y]")).toEqual("3,2,1,1");
   });
   it("deberia devolver la suma de la [*][%]\n1*2%3,7-9  ====>  22", () => {
-    expect(Operacion("//[*][%]\n1*2%3,7-9")).toEqual(22);
+    expect(CalcularCadena("//[*][%]\n1*2%3,7-9")).toEqual(22);
   });
-  // it("deberia calcular para un cantidad", () => {
-  //   expect(calcularTotal(2, 2, "")).toEqual(4);
-  // });
-  // it("deberia calcular para un estado por defecto", () => {
-  //   expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
-  // });
-  // it("deberia calcular para un estado por defecto", () => {
-  //   expect(calcularTotal(3, 2, "CA")).toEqual(6.495);
-  // });
-  // it("deberia calcular para un estado por defecto", () => {
-  //   expect(calcularTotal(3, 2, "UT")).toEqual(6.399);
-  // });
+  it("deberia devolver la suma de la [***][%]\n1***2000%1001,7000-9  ====>  10", () => {
+    expect(CalcularCadena("//[***][%]\n1***2000%1001,7000-9")).toEqual(10);
+  });
 });
 
-function Operacion(cadena)
+function CalcularCadena(cadena)
 {
   if(!(cadena.match("//"))) //si la cadena no coincide con el caracter "//"
     return Sumar(cadena);
@@ -42,64 +30,22 @@ function Operacion(cadena)
     let dividir = cadena.split(["\n"]);
     let delimitadores = dividir[0];
     let cadenaNumeros = dividir[1];
-    //console.log(delimitadores);
-    //console.log(cadenaNumeros);
-    let res = Resultado(cadenaNumeros,delimitadores);
-    //console.log(res);
+    let res = CadenaConDelimitadores(cadenaNumeros,delimitadores);
     return Sumar(res);
-    //return Sumar(Resultado(cadenaNumeros,delimitadores))
-
-    // let dividir = cadena.split(["\n"]);
-    // return dividir[0];
   }
-  
-      
-  //return Sumar(cadenaNumeros)
+
 }
-  //
-function Resultado(cadenaNumeros, delimitadores)
+function CadenaConDelimitadores(cadenaNumeros, delimitadores)
 {
-  //let cadena = cadenaNumeros.join("");
-  let del = delimitadores;
-  console.log("CADENA DE NUMEROS\n"+cadenaNumeros);
-  console.log("DELIMITADORES\n"+del);
-  del = del.replace(/[[]|[/]/gm,"").replace(/(])/gm,",").split(",");
-  // console.log("LIMPIANDO DELIMITADORES\n"+del);
-  // del = del.split(",");
-  del.pop();
-  console.log("LIMPIANDO DELIMITADORES\n"+del);
-  for(let value of del)
+  delimitadores = delimitadores.replace(/[[]|[/]/gm,"").replace(/(])/gm,",").split(",");//Ej: [**][##] ==> **,##
+  delimitadores.pop();
+  for(let value of delimitadores)//convirtiendo los delimitadores en ","
     cadenaNumeros = cadenaNumeros.replace(value, ',');
   console.log("RESULTADO\n" + cadenaNumeros);
   return cadenaNumeros;
 }
 function Sumar(cadena)
 {
-  let reg = /\s*(?:-|,|$)\s*/
-  //let tmp = cadena.split(reg);
-  let tmp = cadena.split(reg).map(i => parseInt(i,10));
-  //var resp = cadena.reduce(reducer)
-  //return tmp;
-  // var tmp1 = [1,2];
+  let tmp = cadena.split(/\s*(?:-|,|$)\s*/).map(i => parseInt(i,10)).filter(i => i <= 1000);
   return tmp.reduce(reducer);
 }
-
-// function delimitador(cadena, arrayDelimitadores){
-//   cadena.replace(/[-]/g, ',');
-//   for (let value of arrayDelimitadores) 
-//     cadena.replace(/value/g, ',');
-// }
-
-
-
-// function impuestoEstado(estado) {
-//   let impuestos = { CA: 0.0825, UT: 0.0665, "": 0 };
-//   return impuestos[estado];
-// }
-
-// function calcularTotal(cantidad, precio, estado) {
-//   let subTotal = cantidad * precio;
-//   let impuesto = impuestoEstado(estado);
-//   subTotal = subTotal + subTotal * impuesto;
-//   return subTotal;
-// }
