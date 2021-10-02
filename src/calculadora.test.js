0291337a5a060e795eb2e2e67bd687b6c88232e7
p48102611 <p481026110@gmail.com>
2021-10-02 01:04:17 -0400

    deberia devolver la suma de la [*][%]\n1*2%3,7-9  ====>  22

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,20 +1,23 @@
 const reducer = (accumulator, curr) => accumulator + curr;
 
 describe("Calculadora ", () => {
-  it("deberia devolver el numero ingresado", () => {
+  it("deberia devolver el numero ingresado 3  ====>  3", () => {
     expect(Operacion("3")).toEqual(3);
   });
-  it("deberia devolver la suma de la cadena", () => {
+  it("deberia devolver la suma de la 1,2  ====>  3", () => {
     expect(Operacion("1,2")).toEqual(3);
   });
-  it("deberia devolver la suma de la cadena", () => {
+  it("deberia devolver la suma de la 1,2-3  ====>  6", () => {
     expect(Operacion("1,2-3")).toEqual(6);
   });
   // it("probando funcion delimitadores", () => {
   //   expect(Resultado("", "[***,hh,y]")).toEqual(["***", "hh", "y"]);
   // });
   it("probando funcion Resultado deberia devolver una cadena separada por comas", () => {
-    expect(Resultado("3***2hh1y1","[***,hh,y]")).toEqual("3,2,1,1");
+    expect(Resultado("3***2hh1y1","[***][hh][y]")).toEqual("3,2,1,1");
+  });
+  it("deberia devolver la suma de la [*][%]\n1*2%3,7-9  ====>  22", () => {
+    expect(Operacion("//[*][%]\n1*2%3,7-9")).toEqual(22);
   });
   // it("deberia calcular para un cantidad", () => {
   //   expect(calcularTotal(2, 2, "")).toEqual(4);
@@ -36,25 +39,42 @@ function Operacion(cadena)
     return Sumar(cadena);
   else
   {
-    delimitadores = cadena.split([/\n/])[0];
-    cadenaNumeros = cadena.split([/\n/])[1];
-    return Sumar(Resultado(cadena,delimitadores))
+    let dividir = cadena.split(["\n"]);
+    let delimitadores = dividir[0];
+    let cadenaNumeros = dividir[1];
+    //console.log(delimitadores);
+    //console.log(cadenaNumeros);
+    let res = Resultado(cadenaNumeros,delimitadores);
+    //console.log(res);
+    return Sumar(res);
+    //return Sumar(Resultado(cadenaNumeros,delimitadores))
+
+    // let dividir = cadena.split(["\n"]);
+    // return dividir[0];
   }
   
       
   //return Sumar(cadenaNumeros)
 }
   //
-function Resultado(cadena, delimitadores)
+function Resultado(cadenaNumeros, delimitadores)
 {
-  let del = delimitadores.replace("//","").replace("]","").replace("[","").split(",");
+  //let cadena = cadenaNumeros.join("");
+  let del = delimitadores;
+  console.log("CADENA DE NUMEROS\n"+cadenaNumeros);
+  console.log("DELIMITADORES\n"+del);
+  del = del.replace(/[[]|[/]/gm,"").replace(/(])/gm,",").split(",");
+  // console.log("LIMPIANDO DELIMITADORES\n"+del);
+  // del = del.split(",");
+  del.pop();
+  console.log("LIMPIANDO DELIMITADORES\n"+del);
   for(let value of del)
-     cadena = cadena.replace(value, ',');
-  return cadena;
+    cadenaNumeros = cadenaNumeros.replace(value, ',');
+  console.log("RESULTADO\n" + cadenaNumeros);
+  return cadenaNumeros;
 }
 function Sumar(cadena)
 {
-
   let reg = /\s*(?:-|,|$)\s*/
   //let tmp = cadena.split(reg);
   let tmp = cadena.split(reg).map(i => parseInt(i,10));
